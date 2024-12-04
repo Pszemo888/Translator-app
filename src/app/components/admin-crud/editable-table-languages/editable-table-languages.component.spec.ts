ca3ca370456aeb3188f17db6b2affe36ca3108d8
Maciek-Sawicki <macieksawickim@gmail.com>
2024-12-05 00:03:53 +0100

    add languages components

---
+++ src/app/components/admin-crud/editable-table-languages/editable-table-languages.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TableLanguagesComponent } from './editable-table-languages.component';
+
+describe('TableLanguagesComponent', () => {
+  let component: TableLanguagesComponent;
+  let fixture: ComponentFixture<TableLanguagesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TableLanguagesComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TableLanguagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -4,6 +4,8 @@ import {FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular
 import { CommonModule } from '@angular/common';
 import { EditableTableTranslationComponent } from '../admin-crud/editable-table-translations/editable-table-translations.component';
 import { AddTranslationComponent } from '../admin-crud/add-translation/add-translation.component';
+import { AddLanguageComponent } from '../admin-crud/add-language/add-language.component';
+import { TableLanguagesComponent } from '../admin-crud/editable-table-languages/editable-table-languages.component';
 
 @Component({
   selector: 'app-admin',
@@ -13,14 +15,20 @@ import { AddTranslationComponent } from '../admin-crud/add-translation/add-trans
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
-
-
   getAdminReadTranslations() {
     return EditableTableTranslationComponent;
   }
 
   getAdminAddTranslations() {
     return AddTranslationComponent;
+  }
+
+  getAdminAddLanguages() {
+    return AddLanguageComponent;
+  }
+
+  getAdminReadLanguages(){
+    return TableLanguagesComponent;
   }
 
 }
